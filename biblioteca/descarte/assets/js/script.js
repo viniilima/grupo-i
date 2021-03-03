0cd54c8b54e0e4b67915a5a630c9c8987e309d42
viniilima <vinii.lima@outlook.com>
2021-03-03 20:04:37 -0300

    Verificação de acervo emprestado implementada
    O acervo só será descartado se o mesmo não estiver emprestado

--- biblioteca/descarte/assets/js/script.js
+++ biblioteca/descarte/assets/js/script.js
@@ -1,3 +1,5 @@
+// Constantes e vars
+const formContainerEl = document.querySelector('.form-container');
 const submitBtn = document.getElementById('submit');
 
 // Functions
@@ -13,16 +15,38 @@ function callDescarte() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if(this.status == 200 && this.readyState == 4) {
+            if(this.responseText == 'empty-input') {
+                errorMessage('Erro! Algum dado não foi inserido :(');
+                return;
+            }
             const successMsg = document.createElement('span');
-            const formContainerEl = document.querySelector('.form-container');
             successMsg.innerHTML = 'Acervo descartado com sucesso :)';
             successMsg.classList.add('success-msg')
             formContainerEl.appendChild(successMsg);
+        }
+        else if(this.status == 201 && this.readyState == 4) {
+            const response = JSON.parse(this.responseText);
+            const data = new Date(response.data_devolucao);
+            errorMessage('O acervo ' + response.id + ' está emprestado no momento.' 
+                        + ' A data de devolução prevista é ' + formatDate(data.getDate()) + '/' + formatDate((data.getMonth()+1)) 
+                        + '/' + data.getFullYear());
         }
     }
     xhr.open('POST', 'php/descarte.php', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send('data=' + (JSON.stringify(data)));
+}
+
+function errorMessage(msg) {
+    const errorMsg = document.createElement('span');
+    const formContainerEl = document.querySelector('.form-container');
+    errorMsg.innerHTML = msg;
+    errorMsg.classList.add('error-msg')
+    formContainerEl.appendChild(errorMsg);
+}
+
+function formatDate(value) {
+    return value < 10 ? '0' + value : value;
 }
 
 // Event Listeners
--- biblioteca/descarte/php/descarte.php
+++ biblioteca/descarte/php/descarte.php
@@ -1,20 +1,49 @@
 <?php
+
 $data = json_decode($_POST['data']);
+
+// Checa se há algum input vazio
+$checkData = get_object_vars($data);
+foreach($checkData as $value) {
+    if($value == '' || $value == null) {
+        http_response_code(200);
+        echo 'empty-input';
+        die();
+    }
+}
+
+// Conexão com o DB
 $conn = mysqli_connect('localhost', 'root', '', 'biblioteca');
 
-// Deleta
+// Checa se o livro está emprestado
+$sql = "SELECT id, data_devolucao FROM emprestimos WHERE id_acervo = $data->id";
+
+if($result = mysqli_query($conn, $sql)) {
+    if(mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result);
+        $emprestimo_data = [
+            'id' => $row['id'],
+            'data_devolucao' => $row['data_devolucao']
+        ];
+        http_response_code(201);
+        echo json_encode($emprestimo_data);
+        die();
+    }
+}
+
+// Deleta o acervo da tabela acervo
 $sql = 'DELETE FROM acervo WHERE id = ' . $data->id;
 
 if(mysqli_query($conn, $sql)) http_response_code(200);
 
-// Adiciona dados na tabela
-$sql = "INSERT INTO `descartes` (id_acervo, data_descarte, motivos, operador) VALUES (
+// Adiciona os dados do acervo descartado na tabela descartes
+$sql = "INSERT INTO descartes (id_acervo, data_descarte, motivos, operador) VALUES (
     $data->id,
     '" . date('Y-m-d') . "',
     '$data->motivos',
     '$data->operador'
 )";
 
-echo $sql;
-
 if(mysqli_query($conn, $sql)) http_response_code(200);
+
+mysqli_close($conn);
